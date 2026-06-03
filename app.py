44c73d311a2d3149206eae08a974f1e272299107
Andrea Scala <andreascalaquattro+github@gmail.com>
2026-06-03 12:38:17 +0200

    Alcune modifiche apportate ad app.py - implementata route /

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, request, render_template, session, url_for
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        session['db_config'] = {
+            'HOST'    : request.form.get('HOST'),
+            'PORT'    : request.form.get('PORT'),
+            'DB'      : request.form.get('DB'),
+            'USERNAME': request.form.get('USERNAME'),
+            'PASSWORD': request.form.get('PASSWORD')
+        }
+        return redirect(url_for('lista'))
+    else:
+        return render_template('index.html')
+    
