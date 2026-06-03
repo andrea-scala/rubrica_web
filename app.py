499491348d04e7f23a493774efd091afe612b544
Andrea Scala <andreascalaquattro+github@gmail.com>
2026-06-03 12:41:22 +0200

    EXTRA: Aggiunti 2 decoratori: per verificare login e che la sessione esista.

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, redirect, request, render_template, session, url_for
 import os
 from dotenv import load_dotenv
+from functools import wraps
 
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
+
+def db_config_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not session.get('db_config'):
+            return redirect(url_for('index'))
+        return f(*args, **kwargs)
+    return decorated
+
+def login_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not session.get('utente'):
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
