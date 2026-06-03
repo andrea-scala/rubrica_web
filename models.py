056a1925b9354d1896a6171174a74566285e6270
Andrea Scala <andreascalaquattro+github@gmail.com>
2026-06-03 11:21:19 +0200

    Aggiunto file models.py

---
+++ models.py
@@ -0,0 +1,7 @@
+class Persona:
+    def __init__(self,id=None,n="",c="",i="",e=0):
+        self.id = id
+        self.nome = n
+        self.cognome = c
+        self.indirizzo = i
+        self.eta = e
